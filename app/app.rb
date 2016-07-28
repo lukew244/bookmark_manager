bda6076dfa5c4a524f8f2bf2def5811b69bda214
lukew244 <luke.webster86@gmail.com>
2016-07-28 22:19:34 +0100

    adds sign in/sign out

--- app/app.rb
+++ app/app.rb
@@ -7,6 +7,7 @@ require 'sinatra/flash'
 class BookmarkManager < Sinatra::Base
 
   register Sinatra::Flash
+  use Rack::MethodOverride
 
   enable :sessions
   set :session_secret, 'super secret'
@@ -49,6 +50,27 @@ class BookmarkManager < Sinatra::Base
       flash.now[:errors] = user.errors.full_messages
       erb :'users/new'
     end
+  end
+
+  get '/sessions/new' do
+    erb :'sessions/new'
+  end
+
+  post '/sessions' do
+    user = User.authenticate(params[:email], params[:password])
+    if user
+      session[:user_id] = user.id
+      redirect to('/links')
+    else
+      flash.now[:errors] = ['The email or password is incorrect']
+      erb :'/sessions/new'
+    end
+  end
+
+  delete '/sessions' do
+    session[:user_id] = nil
+    flash.keep[:notice] = 'goodbye!'
+    redirect to '/links'
   end
 
   helpers do
--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -41,3 +41,46 @@ feature 'User can sign up' do
   end
 
 end
+
+feature 'User can sign in' do
+  let!(:user) do
+    User.create(email: 'user@example.com', password: 'secret1234', password_confirmation: 'secret1234')
+  end
+
+  scenario 'with correct credentials' do
+    sign_in(email: user.email, password: user.password)
+    expect(page).to have_content "Welcome #{user.email}"
+  end
+
+  def sign_in(email:, password:)
+    visit 'sessions/new'
+    fill_in :email, with: email
+    fill_in :password, with: password
+    click_button 'Sign in'
+  end
+
+end
+
+feature 'User signs out' do
+
+  before(:each) do
+    User.create(email: 'test@test.com',
+                password: 'test',
+                password_confirmation: 'test')
+  end
+
+  scenario 'while being signed in' do
+    sign_in(email: 'test@test.com', password: 'test')
+    click_button 'Sign out'
+    expect(page).to have_content('goodbye!')
+    expect(page).not_to have_content('Welcome, test@test.com')
+  end
+
+  def sign_in(email:, password:)
+    visit 'sessions/new'
+    fill_in :email, with: email
+    fill_in :password, with: password
+    click_button 'Sign in'
+  end
+
+end
