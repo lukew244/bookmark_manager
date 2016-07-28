b729c547929089e37a3afeb1fc156c4a7e79f5e5
lukew244 <luke.webster86@gmail.com>
2016-07-28 21:09:53 +0100

    prevents duplicate registrations

--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -17,7 +17,19 @@ feature 'User can sign up' do
   scenario 'with a password that does not match' do
     expect { sign_up(password_confirmation: 'wrong')}.not_to change(User, :count)
     expect(current_path).to(eq('/users'))
-    expect(page).to(have_content("Password and confirmation do not match"))
+    expect(page).to(have_content("Password does not match the confirmation"))
+  end
+
+  scenario 'I can\'t sign up without an email address' do
+    expect { sign_up(email: nil)}.not_to change(User, :count)
+    expect(current_path).to eq('/users')
+    expect(page).to have_content('Email must not be blank')
+  end
+
+  scenario 'I can\'t sign up with an invalid email address' do
+    expect { sign_up(email: 'invalid@email')}.not_to change(User, :count)
+    expect(current_path).to eq('/users')
+    expect(page).to have_content('Email has an invalid format')
   end
 
   def sign_up(email: 'alice@example.com', password: '12345678', password_confirmation: '12345678')
@@ -26,14 +38,6 @@ feature 'User can sign up' do
     fill_in :password, with: password
     fill_in :password_confirmation, with: password_confirmation
     click_button 'Sign up'
-  end
-
-  scenario 'I can\'t sign up without an email address' do
-    expect { sign_up(email: nil)}.not_to change(User, :count)
-  end
-
-  scenario 'I can\'t sign up with an invalid email address' do
-    expect { sign_up(email: 'invalid@email')}.not_to change(User, :count)
   end
 
 end
